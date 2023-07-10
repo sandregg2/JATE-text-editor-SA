717882ac0cc52e5b5ee5938fd66cf60835a7389f
Seth <sandregg2gmail.com>
2023-07-10 18:18:40 -0400

    updated gitignore

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -25,8 +25,13 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      try {
+        console.info('Loaded data from IndexedDB, injecting into editor');
+        const value = data || localData || header;
+        this.editor.setValue(value);
+      } catch(err) {
+        console.log('error', err);
+      }
     });
 
     this.editor.on('change', () => {
